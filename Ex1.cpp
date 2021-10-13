47f42c2c3f94cdcef895b62fb7f3355fff7dce1e
Binary_Assasins <82915997+binaryassasins@users.noreply.github.com>
2021-10-13 20:24:28 +0800

    Add files via upload

---
+++ Ex1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double length, width, area, perimeter;
+
+    cout << "Program to compute and output the perimeter and area of a rectangle." << endl << endl;
+
+    cout << "Input length: ";
+    cin >> length;
+    cout << "Input width: ";
+    cin >> width;
+
+    perimeter = 2 * (length + width);
+    area = length * width;
+
+    cout << "Length = " << length << endl;
+    cout << "Width = " << width << endl;
+    cout << "Perimeter = " << perimeter << endl;
+    cout << "Area = " << area << endl;
+
+    return 0;
+}
